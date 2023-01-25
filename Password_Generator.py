39383ad1ee8342432aa1a1abb7b7a015b3a997a9
Kirti <kirtirajput63969@gmail.com>
2023-01-25 20:10:59 +0530

    testing

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,11 +1,11 @@
-import random
+import random 
 letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 numbers = ["0","1","2","3","4","5","6","7","8","9"]
 symbols = ["!","@","#","$","%","^","&","*","+","-","(",")"]
 print("Welcome to the Password Generator...")
-no_letters = int(input("How many letters would you like in your Password?\n"))
-no_numbers = int(input("How many numbers would you like in your Password?\n"))
-no_symbols = int(input("How many sysmbols would you like in your Password?\n"))
+no_letters = int(input("How many Letters would you like in your Password?\n"))
+no_numbers = int(input("How many Numbers would you like in your Password?\n"))
+no_symbols = int(input("How many Symbols would you like in your Password?\n"))
 
 
 # password = ""
@@ -53,4 +53,4 @@ password = ""
 for char in password_list:
   password += char
 
-print(f"Your Password: {password}")
+print(f"Your Password 🙄: {password}")
