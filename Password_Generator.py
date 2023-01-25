2b00d7560fb5351f2c3cc95355fd655b952c3e64
Kirti <kirtirajput63969@gmail.com>
2023-01-25 17:35:47 +0530

    Testing successfull

--- Password_Generator.py
+++ Password_Generator.py
@@ -5,7 +5,7 @@ symbols = ["!","@","#","$","%","^","&","*","+","-","(",")"]
 print("Welcome to the Password Generator...")
 no_letters = int(input("How many letters would you like in your Password?\n"))
 no_numbers = int(input("How many numbers would you like in your Password?\n"))
-no_symbols = int(input("How many sysmbols would you like in your Password?\n"))
+no_symbols = int(input("How many symbols would you like in your Password?\n"))
 
 
 # password = ""
@@ -53,4 +53,4 @@ password = ""
 for char in password_list:
   password += char
 
-print(f"Your Password: {password}")
+print(f"Your Password is: {password}")
